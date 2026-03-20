79f6acd7b4e9134fde3d2e481292754d6ed6d4e3
Giorgio Cavalli <malebrains@gmail.com>
2026-03-20 13:30:03 +0000

    File changes

--- src/components/BehaviorDetailPopup.jsx
+++ src/components/BehaviorDetailPopup.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { X, CheckCircle2, XCircle } from 'lucide-react';
 import { BEHAVIOR_GUIDE } from '@/lib/behaviorGuide';
 
